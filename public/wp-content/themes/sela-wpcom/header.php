a215d6c4edcd6195013b11896ed634efa3a248eb
Robert Reis <T-RobertR@vulcan.com>
2015-03-12 10:39:39 -0700

    minor style updates

--- public/wp-content/themes/sela-wpcom/header.php
+++ public/wp-content/themes/sela-wpcom/header.php
@@ -15,17 +15,10 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-<!-- KSB added these to match the SeaAroundUs theme header page -->
-<!--
-<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/dropdowns.js"></script>
-<script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/js/util-functions.js"></script>
-<script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/js/saup-functions.js"></script>
--->
-
 <?php wp_enqueue_script("jquery"); ?>
 
 <?php wp_head(); ?>
-<link rel="stylesheet" href="/wp-content/themes/sela-wpcom/overrides.css" type="text/css" media="all" />
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/overrides.css" type="text/css" media="all" />
 </head>
 
 <body <?php body_class(); ?>>
@@ -41,7 +34,7 @@
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 			<?php endif; ?>
 		</div><!-- .site-branding -->
-		
+
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Menu', 'sela' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?> <!-- This part came with the SELA theme and is cool because the highlights will work. -->
--- public/wp-content/themes/sela-wpcom/page-templates/front-page-custom.php
+++ public/wp-content/themes/sela-wpcom/page-templates/front-page-custom.php
@@ -7,9 +7,7 @@
 
 get_header(); ?>
 <?php the_post(); ?>
-<div class="no-img-margin no-h1">
-    <?php get_template_part( 'content', 'page' ); ?>
-</div>
+<?php get_template_part( 'content', 'page' ); ?>
 <?php get_sidebar( 'front-page' ); ?>
 <?php get_sidebar( 'footer' ); ?>
 <?php get_footer(); ?>
