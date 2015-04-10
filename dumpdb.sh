df7ef7a806e8de71f94bf00d8610a178d4659ef9
Robert Reis <T-RobertR@vulcan.com>
2015-04-10 16:20:05 -0700

    update dumpdb script to write directly to cookbook file

--- dumpdb.sh
+++ dumpdb.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-vagrant ssh -c "cd /vagrant && mysqldump -h127.0.0.1 -uroot -ppassword --databases seadb > seadb_dump.sql"
+vagrant ssh -c "cd /vagrant && mysqldump -h127.0.0.1 -uroot -ppassword --databases seadb > cookbook/fluff/files/default/seadb_dump.sql"
