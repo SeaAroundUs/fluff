29187dc06656dc5f37792ba6f22cd6059206aa04
Robert Reis <T-RobertR@vulcan.com>
2015-08-25 15:02:23 -0700

    extra output for invalid token

--- public/wp-content/themes/sela-wpcom/vulcan-custom/shortcodes.php
+++ public/wp-content/themes/sela-wpcom/vulcan-custom/shortcodes.php
@@ -105,7 +105,7 @@ add_shortcode('feedback-form-submit', function($attrs) {
 
     // validation
     if ($_POST['feedback-token'] !== $_SESSION['xrsf']) {
-        wp_die(__('Invalid Token'), 403);
+        wp_die(__('Invalid token: ' . $_POST['feedback-token'] . '!==' . $_SESSION['xrsf']), 403);
     }
 
     // validate & process form
