752b054d8d48706155b2dceaee1c8c37dc9172a6
Robert Reis <T-RobertR@vulcan.com>
2015-09-14 09:52:03 -0700

    [SAU-1680] update wordpress footer

--- public/wp-content/themes/sela-wpcom/footer.php
+++ public/wp-content/themes/sela-wpcom/footer.php
@@ -12,15 +12,9 @@
 
 	<footer id="colophon" class="site-footer">
         <div id="copyright" class="pull-left">
-            © 2015 Sea Around Us
-        </div>
-        <div id="legal" class="pull-right">
+            <span>&copy; 2015 Sea Around Us</span>
+            <a href="/contact/">Contact us</a>
             <a href="/citation-policy/">Citation Policy</a>
-            <!--
-            <a class="" href="#">Privacy Policies</a>
-            <span class="separator">|</span>
-            <a class="" href="#">Terms and Conditions</a>
-            -->
         </div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
