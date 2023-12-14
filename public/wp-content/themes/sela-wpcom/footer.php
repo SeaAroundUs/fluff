8268218edfb39de115efe5bb481d707c4a804c5a
Miel <s.ortiz@q-quatics.org>
2023-12-14 08:32:44 +0800

    Added favicon and GA4 footer
    - Updated favicon
    - Fixed issue with GA4 tags

--- public/wp-content/themes/sela-wpcom/footer.php
+++ public/wp-content/themes/sela-wpcom/footer.php
@@ -27,14 +27,14 @@
 <?php wp_footer(); ?>
 
 <!-- google analytics footer -->
-<!--<script>-->
-    //(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    //    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    //    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    //})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-    //ga('create', 'UA-61452144-2',  { 'cookieDomain': 'none' });
-    //ga('send', 'pageview');
-<!--</script>-->
+<!--<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-61452144-2',  { 'cookieDomain': 'none' });
+    ga('send', 'pageview');
+</script>-->
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <!--<script async src="https://www.googletagmanager.com/gtag/js?id=UA-61452144-2"></script>
